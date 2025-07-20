a5d03bd19a32671540394aa6fe3d20d1c2a17fae
Wiktor Zykubek <dev@wzykubek.xyz>
2025-07-20 20:17:59 +0200

    feat(cmd): add edit command

---
+++ cmd/edit.go
@@ -0,0 +1,63 @@
+package cmd
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(editCmd)
+}
+
+var editCmd = &cobra.Command{
+	Use:   "edit <script_name>",
+	Short: "Edit specified script remotely in default editor.",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		defer c.Close()
+
+		scriptName := args[0]
+		_, lines, err := c.GetScriptLines(scriptName)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		tmpFile, err := os.CreateTemp(os.TempDir(), "sieveman")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		buf := bufio.NewWriter(tmpFile)
+
+		for _, l := range lines {
+			buf.WriteString(l)
+		}
+
+		if err := buf.Flush(); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		editor := os.Getenv("EDITOR")
+		editorProc := exec.Command(editor, tmpFile.Name())
+		editorProc.Stdin = os.Stdin
+		editorProc.Stdout = os.Stdout
+		editorProc.Stderr = os.Stderr
+
+		if err = editorProc.Run(); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		if err = c.PutScript(tmpFile, scriptName); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	},
+}
