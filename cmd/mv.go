0eff058c9d9430c69fc8618204af154f37a532c4
Wiktor Zykubek <dev@wzykubek.xyz>
2025-07-22 14:34:11 +0200

    feat(cmd): add mv command

---
+++ cmd/mv.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(mvCmd)
+}
+
+var mvCmd = &cobra.Command{
+	Use:   "mv <old_name> <new_name>",
+	Short: "Rename script.",
+	Args:  cobra.ExactArgs(2),
+	Run: func(cmd *cobra.Command, args []string) {
+		oldName := args[0]
+		newName := args[1]
+
+		if err := c.RenameScript(oldName, newName); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	},
+}
