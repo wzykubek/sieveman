4b2640851e73f6920c149833c17c002796d2ace2
Wiktor Zykubek <dev@wzykubek.xyz>
2025-07-22 11:47:12 +0200

    feat(cmd): make output_file argument optional in get

--- cmd/get.go
+++ cmd/get.go
@@ -14,16 +14,20 @@ func init() {
 }
 
 var getCmd = &cobra.Command{
-	Use:   "get <script_name> <output_file*>",
+	Use:   "get <script_name> [output_file*]",
 	Short: "Download specified script to given path",
 	Long: `This command downloads specified script and saves it to given location.
 You can use '-' character as file name to print to stdout.`,
-	Args: cobra.ExactArgs(2),
+	Args: cobra.RangeArgs(1, 2),
 	Run: func(cmd *cobra.Command, args []string) {
 		defer c.Close()
 
 		scriptName := args[0]
-		var outFile string = args[1]
+		outFile := scriptName
+		// TODO: Add force flag and do not overwrite existing file
+		if len(args) > 1 {
+			outFile = args[1]
+		}
 
 		_, lines, err := c.GetScriptLines(scriptName)
 		if err != nil {
