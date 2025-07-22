57e657fce65581dce291727280c44fd9222c4d42
Wiktor Zykubek <dev@wzykubek.xyz>
2025-07-22 12:38:56 +0200

    feat(cmd): add rm command

---
+++ cmd/rm.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(rmCmd)
+}
+
+var rmCmd = &cobra.Command{
+	Use:   "rm <script_name>",
+	Short: "Remove a script with the given name",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		scriptName := args[0]
+
+		if err := c.RemoveScript(scriptName); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	},
+}
